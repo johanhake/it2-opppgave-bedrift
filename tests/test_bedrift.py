32dfb455c580f1c4424bf2b1d5bdfc7019a14472
johanhake <johan.hake@gmail.com>
2024-01-15 21:35:49 +0100

    Endret int til float i tester

--- tests/test_bedrift.py
+++ tests/test_bedrift.py
@@ -6,21 +6,21 @@ def test_bedrift_navn():
 
 def test_leder():
     bedrift = Bedrift("Foo")
-    leder = bedrift.legg_til_leder("Arne", 750000)
+    leder = bedrift.legg_til_leder("Arne", 750000.)
     assert len(bedrift.ansatte) == 1
     assert leder.navn == "Arne"
-    assert leder.lønn == 750000
+    assert leder.lønn == 750000.
 
 def test_ansatt():
     bedrift = Bedrift("Foo")
-    leder = bedrift.legg_til_leder("Arne", 750000)
-    ansatt = bedrift.legg_til_ansatt("Lise", 400000, leder.navn)
+    leder = bedrift.legg_til_leder("Arne", 750000.)
+    ansatt = bedrift.legg_til_ansatt("Lise", 400000., leder.navn)
     assert len(bedrift.ansatte) == 2
     assert ansatt.navn == "Lise"
-    assert ansatt.lønn == 400000
+    assert ansatt.lønn == 400000.
 
 def test_leder_lønn():
     bedrift = Bedrift("Foo")
-    leder = bedrift.legg_til_leder("Arne", 750000)
-    bedrift.legg_til_ansatt("Lise", 600000, leder.navn)
+    leder = bedrift.legg_til_leder("Arne", 750000.)
+    bedrift.legg_til_ansatt("Lise", 600000., leder.navn)
     assert leder.lønn >= 600000*1.5
