65c593da798b06ab6489deaed2432009bed8a567
johanhake <johan.hake@gmail.com>
2024-01-15 21:36:19 +0100

    Fikset alle tre klassene Bedrift, Ansatt og Leder, slik at testene ikke failer

--- src/bedrift.py
+++ src/bedrift.py
@@ -1,5 +1,14 @@
 class Ansatt:
     def __init__(self, navn:str, lønn:float, ansattnr:int)->None:
+        if not isinstance(navn, str):
+            raise TypeError(f"Forventet {type('')} for navn, fikk {type(navn)}")
+    
+        if not isinstance(lønn, float):
+            raise TypeError(f"Forventet {type(0.1)} for lønn, fikk {type(lønn)}")
+
+        if not isinstance(ansattnr, int):
+            raise TypeError(f"Forventet {type(1)} for ansattnr, fikk {type(ansattnr)}")
+
         self.navn = navn
         self.lønn = lønn
         self.ansattnr = ansattnr
@@ -9,8 +18,13 @@ class Leder(Ansatt):
         super().__init__(navn, lønn, ansattnr)
         self.ansatte = []
 
-    def legg_til_ansatt(self, ansatt:Ansatt):
-        pass
+    def legg_til_ansatt(self, ny_ansatt:Ansatt):
+        if not isinstance(ny_ansatt, Ansatt):
+            raise TypeError(f"Forventet Ansatt for ansatt, fikk {type(ny_ansatt)}")
+
+        # Må vi endre lønnen?
+        if self.lønn < ny_ansatt.lønn*1.5:
+            self.lønn = ny_ansatt.lønn*1.5
 
 class Bedrift:
     def __init__(self, navn:str)->None:
@@ -18,8 +32,34 @@ class Bedrift:
         self.ansatte = []
 
     def legg_til_ansatt(self, navn:str, lønn:float, leder_navn:str):
-        pass
     
+        if not isinstance(leder_navn, str):
+            raise TypeError(f"Forventet {type('')} for leder_navn, fikk {type(leder_navn)}")
+    
+        # Oppretter Ansatt
+        ny_ansatt = Ansatt(navn, lønn, len(self.ansatte)+1)
+        
+        # Finner lederen
+        leder_ikke_funnet = True
+        for leder in self.ansatte:
+            if isinstance(leder, Leder) and leder.navn == leder_navn:
+                leder_ikke_funnet = False
+                leder.legg_til_ansatt(ny_ansatt)
+        if leder_ikke_funnet:
+            raise ValueError(f"Leder med navn {leder_navn} er ikke registrert.")
+
+        # Legger til den nye ansatte
+        self.ansatte.append(ny_ansatt)
+
+        return ny_ansatt
+        
     def legg_til_leder(self, navn:str, lønn:float):
-        pass
+
+        # Oppretter Leder
+        ny_leder = Leder(navn, lønn, len(self.ansatte)+1)
+        
+        # Legger til den nye ansatte
+        self.ansatte.append(ny_leder)    
+
+        return ny_leder    
     
